432213110dfce4c38167de6be74a8b39f4593da2
alvarosoaress <alvarosoares01@gmail.com>
2025-11-22 15:54:13 -0300

    feat: login

--- index.js
+++ index.js
@@ -7,32 +7,82 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const jar = new CookieJar();
-const session = wrapper(axios.create({ jar, withCredentials: true }));
+const sessions = new Map();
+let session;
 
 const app = express();
 const PORT = process.env.PORT || 3210;
 
 app.use(express.json());
 
-async function login() {
-    try {
-        const url = "https://portaldocliente.praxio.com.br/Home/Entrar";
-        const loginData = {
-            txtLogin: process.env.PORTAL_USER,
-            txtSenha: process.env.PORTAL_PASSWORD,
-            ReturnUrl: ""
-        };
+async function attemptLogin(user, password, maxAttempts = 3) {
+    const existsLogin = sessions.get(user);
 
-        const res = await session.post(url, new URLSearchParams(loginData), {
+    if (existsLogin) {
+        session = existsLogin;
+
+        const validLogin = await validateLogin();
+
+        if (validLogin) return true;
+    }
+
+    const jar = new CookieJar();
+    session = wrapper(axios.create({ jar, withCredentials: true }));
+
+    for (let attempt = 0; attempt < maxAttempts; attempt++) {
+        try {
+            const url = "https://portaldocliente.praxio.com.br/Home/Entrar";
+            const loginData = {
+                txtLogin: user,
+                txtSenha: password,
+                ReturnUrl: ""
+            };
+
+            const res = await session.post(url, new URLSearchParams(loginData), {
+                headers: { "Content-Type": "application/x-www-form-urlencoded" }
+            });
+
+            sessions.set(user, session);
+
+            return res;
+        }
+        catch (error) {
+            console.error("Erro login:", error);
+            return false;
+        }
+    }
+}
+
+async function validateLogin() {
+    try {
+        const url = "https://portaldocliente.praxio.com.br/Ticket/";
+
+        const res = await session.get(url, {
             headers: { "Content-Type": "application/x-www-form-urlencoded" }
         });
 
-        return res;
-    } catch (error) {
-        console.error("Erro login:", error);
-        throw error;
+        if (res.config.url.includes('ReturnUrl')) return false;
+
+        return true;
     }
+    catch (error) {
+        console.error("Erro validateLogin:", error);
+        return false;
+    }
+}
+
+async function login(req, res, next) {
+    if (!req.headers['x-user']) return res.status(403).json('Não informado usuário')
+    if (!req.headers['x-password']) return res.status(403).json('Não informado senha')
+
+    const user = req.headers['x-user'];
+    const password = req.headers['x-password'];
+
+    const loginRes = await attemptLogin(user, password);
+
+    if (!loginRes) return res.status(403).json('Credenciais incorretas.')
+
+    next();
 }
 
 async function tickets(customSearchmenu) {
@@ -75,15 +125,13 @@ async function getCleanTickets(document) {
             newTicket[headers[i]] = cell.innerText.trim();
         });
 
-        ticketsJSON.push(newTicket); // <-- agora no lugar certo
+        ticketsJSON.push(newTicket);
     });
 
     return ticketsJSON;
 }
 
-await login();
-
-app.get('/tickets/:customSearchmenu', async (req, res) => {
+app.get('/tickets/:customSearchmenu', login, async (req, res) => {
     const { customSearchmenu } = req.params;
 
     try {
